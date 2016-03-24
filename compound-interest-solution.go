b4fa118244aca9c909d55d930ea141c44a3534d3
pjd937 <lonnie.webb@capitalone.com>
2016-03-24 12:14:13 -0500

    changes to run on reserved port in examples

--- compound-interest-solution.go
+++ compound-interest-solution.go
@@ -1,29 +1,29 @@
 package main
 
 import (
-    "fmt"
-    "math"
-    "net/http"
-    "net/url"
-    "strconv"
+	"fmt"
+	"math"
+	"net/http"
+	"net/url"
+	"strconv"
 )
 
-func compute_compound_interest( principal float64, rate float64, t_years float64, n_yearly_compounded float64 ) float64 {
-	return principal * math.Pow(( 1 + rate/n_yearly_compounded), (n_yearly_compounded*t_years))
+func compute_compound_interest(principal float64, rate float64, t_years float64, n_yearly_compounded float64) float64 {
+	return principal * math.Pow((1+rate/n_yearly_compounded), (n_yearly_compounded*t_years))
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	values, _ := url.ParseQuery(r.URL.Path[1:])
-	p,_ := strconv.ParseFloat(values["p"][0], 64)
-	rate,_ := strconv.ParseFloat(values["r"][0], 64)
-	t,_ := strconv.ParseFloat(values["t"][0], 64)
-	n,_ := strconv.ParseFloat(values["n"][0], 64)
+	p, _ := strconv.ParseFloat(values["p"][0], 64)
+	rate, _ := strconv.ParseFloat(values["r"][0], 64)
+	t, _ := strconv.ParseFloat(values["t"][0], 64)
+	n, _ := strconv.ParseFloat(values["n"][0], 64)
 	a := compute_compound_interest(p, rate, t, n)
-    fmt.Fprintf(w, "%.02f", a)
+	fmt.Fprintf(w, "%.02f", a)
 }
 
 func main() {
 	fmt.Println("Waiting...")
-    http.HandleFunc("/", handler)
-    http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":9090", nil)
 }
